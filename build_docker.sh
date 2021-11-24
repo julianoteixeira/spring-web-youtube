f5a2a104d559fb427434e5c91b30d1cb7dc8e9fa
guiagb <guilherme.agb94@gmail.com>
2021-11-24 17:22:47 -0300

    subindo alteracoes iniciais

--- build_docker.sh
+++ build_docker.sh
@@ -7,4 +7,4 @@
 #### Pegando ultima tag do git #####
 tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo "Fazendo deploy da tag $tag"
-docker build -t "didox/crud-java-login:$tag" -f Dockerfile .
+docker build -t "guilhermeagb/crud-java-login:$tag" -f Dockerfile .
--- push_docker_hub.sh
+++ push_docker_hub.sh
@@ -1,4 +1,4 @@
 tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 
-docker tag didox/crud-java-login:$tag hub.docker.com/r/didox/crud-java-login:$tag
-docker push didox/crud-java-login:$tag
+docker tag guilhermeagb/crud-java-login:$tag hub.docker.com/r/guilhermeagb/crud-java-login:$tag
+docker push guilhermeagb/crud-java-login:$tag
