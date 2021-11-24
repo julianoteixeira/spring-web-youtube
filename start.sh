e83116a63a589dbe21c4f65f7a8074696bea3e5f
guiagb <guilherme.agb94@gmail.com>
2021-11-24 17:45:02 -0300

    subindo alteracoes iniciais

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ export PASSWORD='040e170e'
 export DATABASE_URL='mysql://um_host_de_base.com/heroku_7f94424f56f1693?reconnect=true'
 
 # ./mvnw spring-boot:run
-
+tag=$( $(git rev-list --tags --max-count=1))
 # docker run -e USER -e PASSWORD -e DATABASE_URL -p 8081:8080 --name crud-java-login didox/crud-java-login
-docker run -d -e USER -e PASSWORD -e DATABASE_URL -p 8081:8080 --name crud-java-login guilhermeagb/crud-java-login:latest
+docker run -d -e USER -e PASSWORD -e DATABASE_URL -p 8081:8080 --name crud-java-login guilhermeagb/crud-java-login:$tag
 docker start crud-java-login
