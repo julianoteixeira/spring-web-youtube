9ea39dbd5b7ff3fa666f1903e33781662a548d4a
guiagb <guilherme.agb94@gmail.com>
2021-11-24 17:45:45 -0300

    subindo alteracoes iniciais

--- push_docker_hub.sh
+++ push_docker_hub.sh
@@ -1,4 +1,4 @@
-tag=$( $(git rev-list --tags --max-count=1))
+tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 docker tag guilhermeagb/crud-java-login:$tag hub.docker.com/r/guilhermeagb/crud-java-login:$tag
 docker push guilhermeagb/crud-java-login:$tag
