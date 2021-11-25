23f0e5034d770f658d55247b4136269575549039
julianoteixeira <68123163+julianoteixeira@users.noreply.github.com>
2021-11-25 15:56:43 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ export DATABASE_URL='mysql://um_host_de_base.com/heroku_7f94424f56f1693?reconnec
 # ./mvnw spring-boot:run
 tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 # docker run -e USER -e PASSWORD -e DATABASE_URL -p 8081:8080 --name crud-java-login julianodocker1234/crud-java-login
+systemctl restart docker
 docker run -d -e USER -e PASSWORD -e DATABASE_URL -p 8081:8080 --name crud-java-login julianodocker1234/crud-java-login:$tag
 #docker start crud-java-login
